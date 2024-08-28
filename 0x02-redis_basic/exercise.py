e922e08980aed877dc943fbbc7f8a869a386f251
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-08-28 15:53:09 +0100

    Done with task #3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,6 +16,20 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """
+    Stores the history of calls for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.rpush(f"{key}:inputs", str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(f"{key}:outputs", str(output))
+        return output
+    return wrapper
+
+
 
 class Cache:
     """A cache class
@@ -27,6 +41,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores new data into the cache storage
         """
