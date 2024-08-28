f6c82d328ac713b2b579513918d364c0506dd3bf
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-08-28 15:14:19 +0100

    Done task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Using redis in an ALX Project
 """
-from typing import Union
+from typing import Union, Optional, Callable, Any
 import uuid
 import redis
 
@@ -21,3 +21,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Gets a value from the cache and optionally converts it
+                to it's original value using the callable argument "fn"
+        """
+        try:
+            res = self._redis.get(key)
+            if fn:
+                res = fn(res)
+            return res
+        except:
+            return None
+
+    def get_str(self, key: str):
+        """Automatically decode data gotten from cache as a string"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """
+        Automatically decode data gotten from cache as a string
+        """
+        return self.get(key, int)
